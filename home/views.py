bd67626d04ba745856a956c8e06f21648fb5c7c4
ashur-k <ashurkanwal@yahoo.com>
2020-10-11 14:32:16 +0000

    added home setting model and added contact and about us template created and updated  views for new models and templates

--- home/views.py
+++ home/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from home.models import Setting
 
 
 # Create your views here.
 def index(request):
-    university = "something"
-    dept = "some department"
+    setting = Setting.objects.get(pk=1)
+    page = "home"
     context = {
-        'university': university,
-        'department': dept
+        'setting': setting,
+        'page': page
     }
     return render(request, 'index.html', context)
+
+
+def aboutus(request):
+    setting = Setting.objects.get(pk=1)
+    context = {
+            'setting': setting,
+        }
+    return render(request, 'about.html', context)
+
+
+def contactus(request):
+    setting = Setting.objects.get(pk=1)
+    context = {
+        'setting': setting,
+    }
+    return render(request, 'contactus.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from home import views
+
 urlpatterns = [
     path('', include('home.urls')),
+    path('about/', views.aboutus, name='aboutus'),
+    path('contact/', views.contactus, name='contactus'),
     path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
