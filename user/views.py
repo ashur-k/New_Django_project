8b91051c5e876a0e795bb0c6c83823b0998f4fef
ashur-k <ashurkanwal@yahoo.com>
2020-10-19 17:24:07 +0000

    User profile login/logout created

--- user/views.py
+++ user/views.py
@@ -1,7 +1,45 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from product.models import Category
+from user.models import UserProfile
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("User app is working")
+
+
+def login_form(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            current_user = request.user
+            userprofile = UserProfile.objects.get(user_id=current_user.id)
+            request.session['userimage'] = userprofile.image.url
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, "Login Error !! Username or Password is incorrect")
+
+    category = Category.objects.all()
+    context = {
+        'category': category
+    }
+    return render(request, 'login_form.html', context)
+
+
+def signup_form(request):
+    category = Category.objects.all()
+    context = {
+        'category': category
+    }
+    render(request, 'login_form.html', context)
+
+
+def logout_func(request):
+    logout(request)
+    return HttpResponseRedirect('/')
